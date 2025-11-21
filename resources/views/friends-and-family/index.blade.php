8ba799de2b457c8b5ac20ead793a60f8df7e90ef
Doracotuz <edgar.igs92@gmail.com>
2025-11-21 01:09:54 -0600

    Cambios de vista

--- resources/views/friends-and-family/index.blade.php
+++ resources/views/friends-and-family/index.blade.php
@@ -1,8 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Friends & Family') }}
-        </h2>
+        <div class="flex flex-col md:flex-row justify-between items-end">
+            <div class="mb-2 md:mb-0">
+                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+                    Bienvenido, 
+                    <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">
+                        {{ Auth::user()->name }}
+                    </span>
+                </h2>
+                <p class="mt-2 text-md text-gray-500 font-medium">
+                    Aquí se muestra el panel general para la gestión de tu operación.
+                </p>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
@@ -13,12 +23,12 @@
                 <a href="{{ route('ff.sales.index') }}" 
                    class="group relative md:col-span-2 md:row-span-2 rounded-lg overflow-hidden shadow-lg transition-all duration-500 ease-in-out hover:shadow-2xl hover:-translate-y-2">
                     
-                    <img src="https://cdnx.jumpseller.com/la-vinateria/image/63870582/Captura_de_pantalla_2025-05-28_a_la_s__3.14.58_p.m..png?1748459725" 
+                    <img src="https://akasia.com.mx/blog/wp-content/uploads/imagenb.png" 
                          alt="Venta" 
                          class="absolute inset-0 h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110">
                     
                     <div class="absolute bottom-0 left-0 right-0 p-5 bg-white/80 backdrop-blur-sm shadow-inner-t">
-                        <h3 class="text-2xl font-semibold text-gray-900">Punto de Venta</h3>
+                        <h3 class="text-2xl font-semibold text-gray-900">Captura de pedidos</h3>
                         
                         <div class="overflow-hidden transition-all duration-500 ease-in-out max-h-0 group-hover:max-h-40">
                             <p class="text-sm text-gray-600 mt-2 font-light">
@@ -31,7 +41,7 @@
                 <a href="{{ route('ff.inventory.index') }}" 
                    class="group relative md:col-span-2 rounded-lg overflow-hidden shadow-lg transition-all duration-500 ease-in-out hover:shadow-2xl hover:-translate-y-2">
                     
-                    <img src="https://bsmedia.business-standard.com/_media/bs/img/article/2025-05/14/full/1747220660-1474.jpg" 
+                    <img src="https://www.beetrack.com/hs-fs/hubfs/Fotos%20Blog/manager-of-warehouse-check-inventory-with-laptop-w-2021-08-29-22-34-00-utc.jpg" 
                          alt="Inventario" 
                          class="absolute inset-0 h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110">
                     
@@ -48,7 +58,7 @@
                 <a href="{{ route('ff.catalog.index') }}" 
                    class="group relative rounded-lg overflow-hidden shadow-lg transition-all duration-500 ease-in-out hover:shadow-2xl hover:-translate-y-2">
                     
-                    <img src="https://www.dondeir.com/wp-content/uploads/2024/10/nahual-bebidas-para-dia-de-muertos-con-moet-hennessy.jpg" 
+                    <img src="https://blog.agenciaclepsidra.com/hubfs/person-using-laptop-computer-during-daytime-196655.jpg" 
                          alt="Catálogo" 
                          class="absolute inset-0 h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110">
                     
@@ -65,7 +75,7 @@
                 <a href="{{ route('ff.reports.index') }}" 
                 class="group relative rounded-lg overflow-hidden shadow-lg transition-all duration-500 ease-in-out hover:shadow-2xl hover:-translate-y-2">
                                     
-                    <img src="https://vino-joy.com/wp-content/uploads/2025/05/MOET-HENNESSY-IMAGE.jpg" 
+                    <img src="https://www.grupocibernos.com/hubfs/42.png" 
                         alt="Reportes" 
                         class="absolute inset-0 h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110">
                     
@@ -75,7 +85,7 @@
                         </h3>
                         <div class="overflow-hidden transition-all duration-500 ease-in-out max-h-0 group-hover:max-h-40">
                             <p class="text-sm text-gray-600 mt-2 font-light">
-                                Análisis y métricas del evento.
+                                Análisis y métricas de la operación.
                             </p>
                         </div>
                     </div>
