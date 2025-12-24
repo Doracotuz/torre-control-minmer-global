a70fcec7683d0b418018ecc675d1aab171553ffa
Ismael <i.sanchez@minmerglobal.com>
2025-12-24 14:14:14 -0600

    Cambios en apline

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
 
 import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
 
 import OrgChart from 'orgchart';
-
+Alpine.plugin(collapse);
 window.Alpine = Alpine;
 
 Alpine.start();
